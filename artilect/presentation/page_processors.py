ec4f4fae7d3dedaf938469be6f51fcba3731fb7a
Mathias Ettinger <mathias@mettinger.fr>
2015-06-21 22:45:01 +0200

    Fix page_processors not being spelled correctly

--- artilect/presentation/page_processors.py
+++ artilect/presentation/page_processors.py
@@ -2,11 +2,11 @@ from __future__ import unicode_literals
 from mezzanine.conf import settings
 from mezzanine.pages.page_processors import processor_for
 from mezzanine.utils.views import paginate
-from wiki.models import Wiki
+from presentation.models import PresentationCategory
 
-@processor_for(Wiki)
+@processor_for(PresentationCategory)
 def wiki_processor(request, page):
-    wiki = page.wiki
-    qs = paginate(wiki.articles.all(), request.GET.get("page", 1),
+    cat = page.presentationcategory
+    qs = paginate(cat.presentations.published(), request.GET.get("page", 1),
         settings.ITEMS_PER_PAGE, settings.MAX_PAGING_LINKS)
     return {"this_category_list": qs}
--- artilect/wiki/page_processors.py
+++ artilect/wiki/page_processors.py
@@ -2,11 +2,11 @@ from __future__ import unicode_literals
 from mezzanine.conf import settings
 from mezzanine.pages.page_processors import processor_for
 from mezzanine.utils.views import paginate
-from presentation.models import PresentationCategory
+from wiki.models import Wiki
 
 @processor_for(Wiki)
 def wiki_processor(request, page):
-    cat = page.presentationcategory
-    qs = paginate(cat.presentations.published(), request.GET.get("page", 1),
+    wiki = page.wiki
+    qs = paginate(wiki.articles.published(), request.GET.get("page", 1),
         settings.ITEMS_PER_PAGE, settings.MAX_PAGING_LINKS)
     return {"this_category_list": qs}
